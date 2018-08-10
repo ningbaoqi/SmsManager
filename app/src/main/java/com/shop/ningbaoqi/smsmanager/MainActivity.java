459faae1d8b31f4572cb69f66361eeb771bd8c95
ningbaoqi <baoqi.ning@new-bund.net>
2018-08-10 17:23:20 +0800

    发送短信

--- app/src/main/java/com/shop/ningbaoqi/smsmanager/MainActivity.java
+++ app/src/main/java/com/shop/ningbaoqi/smsmanager/MainActivity.java
@@ -2,6 +2,10 @@ package com.shop.ningbaoqi.smsmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 发送短信
+     *
+     * @param view
+     */
+    public void send(View view) {
+        SmsManager manager = SmsManager.getDefault();
+        /**
+         * 如果短信太长 可以切割短信
+         * */
+        ArrayList<String> contents = manager.divideMessage("dadahkujfhSFEHfoSGFJKGFLKSADGF,GBljkDGFKGfldsagfKJGFSDKJFGLKGFkhGFKFGKJFG");
+        for (String string : contents) {
+            manager.sendTextMessage("13823194744", null, string, null, null);
+        }
     }
 }
